460380245bebb40d6be8adb5ca1c02dcb65f1ede
Daniel Morala <morala84@gmail.com>
2020-07-08 20:19:31 +0200

    Programa de gestión de un teatro.

---
+++ Actividad_7/src/com/itt/arte/Local.java
@@ -0,0 +1,50 @@
+package com.itt.arte;
+
+public class Local {
+
+    private String domicilio;
+    private int metros;
+    private int accesos;
+
+    public local() {
+    }
+
+    public local(String domicilio, int metros, int accesos) {
+        this.domicilio = domicilio;
+        this.metros = metros;
+        this.accesos = accesos;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public int getMetros() {
+        return metros;
+    }
+
+    public void setMetros(int metros) {
+        this.metros = metros;
+    }
+
+    public int getAccesos() {
+        return accesos;
+    }
+
+    public void setAccesos(int accesos) {
+        this.accesos = accesos;
+    }
+
+    @Override
+    public String toString() {
+        return "local [accesos=" + accesos + ", domicilio=" + domicilio + ", metros=" + metros + "]";
+    }
+
+    
+    
+    
+}
